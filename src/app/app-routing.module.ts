0cf1715a6b964f71e91fdf88ba14fd94b9b5ae15
Jakub112358 <jakubszymanski@hotmail.com>
2023-03-07 19:54:56 +0100

    added header (nav bar) and radio button in start-menu component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StartMenuComponent} from "./components/start-menu/start-menu.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: StartMenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/start-menu/start-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-menu',
+  templateUrl: './start-menu.component.html',
+  styleUrls: ['./start-menu.component.css']
+})
+export class StartMenuComponent {
+}
